03dd61ade00a2ca73a83caba83c08327148d379e
EwerssonSlv <ewerssonsllv@gmail.com>
2025-01-02 23:48:28 -0300

    Last Commit

--- src/main/kotlin/com/ewersson/netflix_api_app/repository/SimilarMovieRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.ewersson.netflix_api_app.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface SimilarMovieRepository : JpaRepository<SimilarMovie, Int>
